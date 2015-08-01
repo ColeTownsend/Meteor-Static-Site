5d12287dc55c6002420e1763eaa278b176b68edc
Cole Townsend <cole@coletownsend.com>
2015-08-01 09:41:44 -0400

    adding journal types.

--- client/views/journal/journals.js
+++ client/views/journal/journals.js
@@ -1,11 +1,13 @@
 PostList = [{
     title: 'Fresh Install Yosemite To Development: The Easy Way',
     postType: "post",
+    subtitle: "Guide",
     info: "This requires being comfortable in the command line. You’ve been warned. This is also opinionated. But it'll get you set up in just a couple hours.",
     template: 'fresh-install-yosemite-easy'
 }, {
     title: "Tour de France Vis",
     postType: "link",
+    subtitle: "School Project",
     info: "We've gathered from the Tour de France website over 100 years of rider data as well as the last 3 tours in routes, and processed it in python.",
     link: "http://coletownsend.github.io/ben-cole-project/"
 }]
